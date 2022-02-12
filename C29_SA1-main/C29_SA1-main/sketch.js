8761c04e24f3fee8b138ff9f82b9d94ef5b1b8fb
Coolboy145 <88191598+Coolboy145@users.noreply.github.com>
2022-02-12 17:54:04 +0000

    Add files via upload

---
+++ C29_SA1-main/C29_SA1-main/sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var rope;
+var fruit;
+var con;
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+
+  rope = new Rope(9,{x:250,y:50})
+
+  var fruit_options = {
+    density:0.001
+  }
+  fruit = Bodies.circle(300,300,15,fruit_options)
+  Matter.Composite.add(rope.body,fruit)
+
+  con = new Link(rope,fruit)
+
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope.show()
+  Engine.update(engine);
+  ellipse(fruit.position.x,fruit.position.y,15,15)
+
+ 
+   
+}
